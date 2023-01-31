ef09550c0ed7ebc4a0c056a3df37b62ebc9e374c
Christian Yang <kychris1999@hotmail.com>
2023-01-30 22:15:04 -0800

    Create 3d graphs

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -3,18 +3,29 @@ using static UnityEngine.Mathf;
 
 public static class FunctionLibrary
 {
-    public static float Wave(float x, float t) {
-        return Sin(PI * (x + t));
+    public delegate float Function (float x, float z, float t);
+
+    public enum FunctionName { Wave, MultiWave, Ripple }
+    private static Function[] functions = { Wave, MultiWave, Ripple};
+
+
+    public static Function GetFunction (FunctionName name) {
+        return functions[(int) name];
     }
 
-    public static float MultiWave (float x, float t) {
+    public static float Wave(float x, float z, float t) {
+        return Sin(PI * (x + z + t));
+    }
+
+    public static float MultiWave (float x, float z, float t) {
         float y = Sin(PI * (x + 0.5f * t));
-        y += Sin(2f * PI * (x + t)) * 0.5f;
-        return y * (2f / 3f);
+        y += Sin(2f * PI * (z + t)) * 0.5f;
+        y += Sin(PI * (x + z + 0.25f * t));
+        return y * (1f / 2.5f);
     }
 
-    public static float Ripple (float x, float t) {
-        float d = Abs(x);
+    public static float Ripple (float x, float z, float t) {
+        float d = Sqrt(x * x + z * z);
         float y = Sin(PI * (4f * d - t));
         return y / (1f + 10f * d);
     }
