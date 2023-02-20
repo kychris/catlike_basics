55e5c3d65591277ac7c82034d32d6393098358d7
Christian Yang <kychris1999@hotmail.com>
2023-02-20 12:00:36 -0800

    Create and load midi number conversion json

--- Assets/Scripts/SceneScripts/Keyboard.cs
+++ Assets/Scripts/SceneScripts/Keyboard.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Linq;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System;
 
 public class Keyboard : MonoBehaviour
 {
     public TMP_Text textField;
-
+    public TextAsset midiConversionText;
 
     string[] components = new string[] { "", "", "" };
 
+    private void Start()
+    {
+        LoadJson();
+    }
+
     public void ChangeNote(ButtonType type, string c)
     {
-        components[(int)type] = c;
+        // Remove accidentals if seeing _
+        if (c == "_")
+        {
+            components[2] = "";
+        }
+        else
+        {
+            components[(int)type] = c;
+        }
         updateText();
     }
 
@@ -21,4 +37,19 @@ public class Keyboard : MonoBehaviour
     {
         textField.text = string.Join("", components);
     }
+
+    void LoadJson()
+    {
+        string json = midiConversionText.text;
+        Dictionary<string, int> data = JsonConvert.DeserializeObject<Dictionary<string, int>>(json);
+        foreach (KeyValuePair<string, int> kvp in data)
+        {
+            Debug.Log(kvp.Key + " " + kvp.Value);
+        }
+    }
+}
+
+class MidiNum
+{
+    public string name;
 }
